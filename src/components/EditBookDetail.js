fe38d4b0daa9f3a5d59b7a8a7c0c7fb1aa5fff15
jaydeepparmar2244 <jaydeep2244.67@gmail.com>
2022-05-18 23:26:52 +0530

    material ui styling of edit pages done

--- src/components/EditBookDetail.js
+++ src/components/EditBookDetail.js
@@ -1,3 +1,5 @@
+import { Button, Grid, InputLabel, MenuItem, MenuList, Select, TextField } from '@mui/material'
+import { Container } from '@mui/system'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -13,20 +15,20 @@ export const EditBookDetail = () => {
     const [students, setstudents] = useState([])
     const navigate = useNavigate()
 
-    const getStudents = () =>{
-        axios.get('http://localhost:8080/students').then(res=>{
+    const getStudents = () => {
+        axios.get('http://localhost:8080/students').then(res => {
             setstudents(res.data)
             console.log(res.data)
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err)
         })
     }
 
-    const getBookDetail = () =>{
-        axios.get(`http://localhost:8080/books/${id}`).then(res=>{
+    const getBookDetail = () => {
+        axios.get(`http://localhost:8080/books/${id}`).then(res => {
             console.log(res.data)
             setbook(res.data)
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err)
         })
     }
@@ -34,49 +36,80 @@ export const EditBookDetail = () => {
         getStudents();
         getBookDetail();
     }, [])
-    
 
-    const updateData = (e) =>{
+
+    const updateData = (e) => {
         e.preventDefault();
         var data = {
-            bookName:bookName,
-            author:author,
-            borrowDate:borrowDate,
-            borrowedBy:borrowedBy,
-            returnDate:returnDate
+            bookName: bookName,
+            author: author,
+            borrowDate: borrowDate,
+            borrowedBy: borrowedBy,
+            returnDate: returnDate
         }
-        axios.put(`http://localhost:8080/books/${id}`,data).then(res=>{
+        axios.put(`http://localhost:8080/books/${id}`, data).then(res => {
             console.log(res.data)
             navigate('/books')
-        }).catch(err=>{
+        }).catch(err => {
             console.log(err)
         })
     }
 
-  return (
-    <div>
-        <form onSubmit={updateData}>
-            <label htmlFor="bookName">Book Name</label>
-            <input type="text" id='bookName' name='bookName' onChange={(e)=>{setbookName(e.target.value)}} defaultValue={book.bookName} />
-            <label htmlFor="author">Author Name</label>
-            <input type="text" name="author" id="author" onChange={(e)=>{setauthor(e.target.value)}} defaultValue={book.author} />
-            <label htmlFor="borrowedBy">Borrowed By</label>
-            <select name="borrowedBy" id="borrowedBy" onChange={(e)=>{setborrowedBy(e.target.value)}}>
-                {
-                    students.map(student=>{
-                        return(
-                            <option value={student.id}>{student.firstName}</option>           
-                        )
-                    })
-                }
-            </select>
-            {/* <input type="text" name='borrowedBy' id='borrowedBy' onChange={(e)=>{setborrowedBy(e.target.value)}} defaultValue={book.borrowedBy} /> */}
-            <label htmlFor="borrowDate">Borrowed Date</label>
-            <input type='date' id='borrowDate' name='borrowDate' onChange={(e)=>{setborrowDate(e.target.value)}} defaultValue={book.borrowDate} />
-            <label htmlFor="returnDate">Expected Return Date</label>
-            <input type="date" name="returnDate" id="returnDate" onChange={(e)=>{setreturnDate(e.target.value)}} defaultValue={book.returnDate} />
-            <button type='submit'>Update Data</button>
-        </form>
-    </div>
-  )
+    return (
+        <Container sx={{ margin: 'auto', p: 6 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <form onSubmit={updateData}>
+                        <div style={{ margin: '25px' }}>
+                            <label htmlFor="bookName" style={{ marginRight: '35px' }}>Book Name</label>
+                            <TextField
+                                id="outlined-helperText"
+                                label={book.bookName}
+                                name='bookName'
+                                onChange={(e) => { setbookName(e.target.value) }}
+                            />
+                        </div>
+                        <div style={{ margin: '25px' }}>
+                            <label htmlFor="author" style={{ marginRight: '20px' }}>Author Name</label>
+                            <TextField
+                                id="outlined-helperText"
+                                label={book.author}
+                                name='author'
+                                onChange={(e) => { setauthor(e.target.value) }}
+                            />
+                        </div>
+                        <div style={{ margin: '25px' }}>
+                        <label htmlFor="borrowedBy" style={{ marginRight: '25px' }}>Borrowed By</label>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label={book.borrowedBy}
+                            name="borrowedBy"
+                            onChange={(e) => { setborrowedBy(e.target.value) }}
+                        >
+                            {
+                                students.map(student => {
+                                    return (
+                                        <MenuItem value={student.id}>{student.firstName}</MenuItem>
+                                    )
+                                })
+                            }
+                        </Select>
+                        </div>
+                        <div style={{ margin: '25px' }}>
+                        <label htmlFor="borrowDate" style={{ marginRight: '25px' }}>Borrow Date</label>
+                        <input type='date' id='borrowDate' name='borrowDate' onChange={(e) => { setborrowDate(e.target.value) }} defaultValue={book.borrowDate} />
+                        </div>
+                        <div style={{ margin: '25px' }}>
+                        <label htmlFor="returnDate" style={{ marginRight: '28px' }}>Return Date</label>
+                        <input type="date" name="returnDate" id="returnDate" onChange={(e) => { setreturnDate(e.target.value) }} defaultValue={book.returnDate} />
+                        </div>
+                        <div style={{ margin: '25px' }}>
+                        <Button variant="contained" type="submit">Update Data</Button>
+                        </div>
+                    </form>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
--- src/components/EditStudentDetail.js
+++ src/components/EditStudentDetail.js
@@ -1,34 +1,73 @@
+import { Button, Grid, TextField } from '@mui/material'
+import { Container } from '@mui/system'
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const EditStudentDetail = () => {
+  const [student, setstudent] = useState('')
   const id = useParams().id
-  const [firstName, setfirstName] = useState('')
-  const [lastName, setlastName] = useState('')
+  const [firstName, setfirstName] = useState(student.firstName)
+  const [lastName, setlastName] = useState(student.lastName)
 
-  const updateData = (e) =>{
-    e.preventDefault();
-    var data = {
-      firstName:firstName,
-      lastName:lastName
-    }
-    axios.put(`http://localhost:8080/students/${id}`,data).then(res=>{
-       console.log(res.data)
+  const getStudentDetail = () =>{
+    axios.get(`http://localhost:8080/students/${id}`).then(res=>{
+      console.log(res.data)
+      setstudent(res.data)
     }).catch(err=>{
       console.log(err)
     })
   }
 
+  useEffect(() => {
+    getStudentDetail()
+  }, [])
+  
+
+  const updateData = (e) => {
+    e.preventDefault();
+    var data = {
+      firstName: firstName,
+      lastName: lastName
+    }
+    axios.put(`http://localhost:8080/students/${id}`, data).then(res => {
+      console.log(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
   return (
-    <div>
-      <form onSubmit={updateData}>
-        <label htmlFor="firstName"> First Name</label>
-        <input type="text" id='firstName' name='firstName' onChange={(e)=>{setfirstName(e.target.value)}} />
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" id='lastName' name='lastName'onChange={(e)=>{setlastName(e.target.value)}}/>
-        <button type='submit'>Update data</button>
-      </form>
-    </div>
+    <Container sx={{ margin: 'auto', p: 6 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <form onSubmit={updateData}>
+            <div style={{ margin: '25px' }}>
+              <label htmlFor="firstName" style={{ marginRight: '35px' }}> First Name</label>
+              <TextField
+                id="outlined-helperText"
+                label={student.firstName}
+                name='firstName'
+                defaultValue={student.firstName}
+                onChange={(e) => { setfirstName(e.target.value) }}
+              />
+            </div>
+            <div style={{ margin: '25px' }}>
+              <label htmlFor="lastName" style={{ marginRight: '35px' }}>Last Name</label>
+              <TextField
+                id="outlined-helperText"
+                label={student.lastName}
+                name='lastName'
+                defaultValue={student.lastName}
+                onChange={(e) => { setlastName(e.target.value) }}
+              />
+            </div>
+            <div style={{ margin: '25px' }}>
+              <Button variant="contained" type="submit">Update Data</Button>
+            </div>
+          </form>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
